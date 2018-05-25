e5e07f9ff7bee80612a8663fceb3409bd857f8b1
Aaron Reid <reid@janatechnology.com>
2018-05-25 10:33:46 -0400

    Made code more dynamic

--- letterstrings.py
+++ letterstrings.py
@@ -17,20 +17,45 @@ import math
 #    lettering += chr(i%26+65)
 #    print(lettering)
 
-for i in range(0,1400):
+#for i in range(0,720):
+#    lettering = ''
+#    if i < 25:
+#        lettering += chr(i%26+65)
+#        print(lettering)
+#    else:
+#        o = 0
+#        if i != 26 and int(i/(676+26)) < 0:
+#            for j in range(0, int(math.log(i,26))):
+#                o += 26**j #* (int(math.log(i,26)) + 1)
+#        for j in range(0, int(math.log(i-o,26))): # get number of letters
+#            k = int(i/26**(j+1)) - 1
+#            lettering = chr(k%26+65) + lettering
+#        lettering += chr(i%26+65)
+##        print('i is: ' + chr(i%26+65))
+#        print(lettering)
+#k = -1
+#for j in range(0,3):
+#    k += 26**j
+
+
+for i in range(0, 73):
     lettering = ''
-    if i < 25:
+    kp = 0
+    k = []
+    n2 = 0
+    if i < 26:
         lettering += chr(i%26+65)
         print(lettering)
     else:
-        o = 0
-        if i != 26 and int(i/(676+26)) < 0:
-            for j in range(0, int(math.log(i,26))):
-                o += 26**j #* (int(math.log(i,26)) + 1)
-        for j in range(0, int(math.log(i-o,26))): # get number of letters
-            k = int(i/26**(j+1)) - 1       
-            lettering = chr(k%26+65) + lettering
+        n = int(math.log(i, 26))
+        for j in range(0, n):
+            kp += 26**(j+1)
+            if i >= kp:
+                k.append(kp)
+        for j in range(0, len(k)):
+            n2 = int(math.log(k[j], 26))
+            o = int(i/k[j]) - 1
+            lettering = chr(o%26+65) + lettering
         lettering += chr(i%26+65)
-#        print('i is: ' + chr(i%26+65))
         print(lettering)
-    
+#    print(k)
